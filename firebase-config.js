7f198ddf15982bb629e055cfa7c0aa604f1114bf
luviginny <luviginreminajes@gmail.com>
2026-08-09 22:15:43 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Paste your Firebase Web App config here. See SETUP.txt.
+window.FIREBASE_CONFIG={
+ apiKey:"PASTE_YOUR_API_KEY_HERE",
+ authDomain:"PASTE_YOUR_PROJECT_ID.firebaseapp.com",
+ databaseURL:"https://PASTE_YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+ projectId:"PASTE_YOUR_PROJECT_ID",
+ storageBucket:"PASTE_YOUR_PROJECT_ID.firebasestorage.app",
+ messagingSenderId:"PASTE_YOUR_MESSAGING_SENDER_ID",
+ appId:"PASTE_YOUR_APP_ID"
+};
